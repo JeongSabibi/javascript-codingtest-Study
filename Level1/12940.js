c33e939fad401055d3efcd0ed5d935056178c3a2
JeongSabibi <jsbb528@naver.com>
2024-04-02 21:52:00 +0900

    Upload week3

--- Level1/12940.js
+++ Level1/12940.js
@@ -1,3 +1,12 @@
 /**
  * 최대공약수와 최소공배수
  */
+function solution(n, m) {
+  let maxDivideNum;
+  for (let i = 1; i <= Math.min(n, m); i++) {
+    if (Math.max(n, m) % i === 0 && Math.min(n, m) % i === 0) maxDivideNum = i;
+  }
+
+  const answer = [maxDivideNum, (n * m) / maxDivideNum];
+  return answer;
+}
